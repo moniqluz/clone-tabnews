488ef15f60d6cc2c206026dfcbd38f242328da8c
moniqluz <101468998+moniqluz@users.noreply.github.com>
2024-01-30 03:16:52 +0000

    modifica a frase para o desafio

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
 function Home() {
-    return <h1>Uuuu lalalá... Ai lóviu bêibe</h1>
+    return <h1>Promoção!!!  Por apenas um abracito fofito, você leva uma MASSAGEM NO PÉ! Mas aproveite que é só hoje.</h1>
 }
 export default Home;
